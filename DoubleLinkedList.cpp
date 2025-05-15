701b1a03b592bdbcca688c989f527ce6253cf918
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-15 10:25:32 +0700

    menambahkan class void hapus

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -73,4 +73,11 @@ class DoubleLinkedList{
 
             current->next = newNode; // Step 9d: current.next = newNode
         }
+
+        void hapus(){
+            if (START == NULL){
+                cout << "\nList is empty" << endl;
+                return;
+            }
+        }
 };
