d28eaddad0d9b9b7ba18ae5743430263e119b756
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-15 10:47:10 +0700

    menambahkan fungsi void traverse

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -113,4 +113,12 @@ class DoubleLinkedList{
             delete current;
             cout << "Record with roll number " << rollNo << " deleted" << endl;
         }
+
+        void traverse(){
+            if (START == NULL){
+                cout << "\nList is empty" << endl;
+                return;
+            }
+
+        }
 };
